fc7b2c740c67b7bc270942ae90a71a7675280510
aayus karki <karki.aayush2003@icloud.com>
2025-12-03 13:05:17 +0545

    feat(auth): add login endpoint to user controller

--- src/api/user/userController.ts
+++ src/api/user/userController.ts
@@ -1,5 +1,5 @@
 import type { Request, RequestHandler, Response } from "express";
-import { CreateUserSchema, UserResponse, type User } from "./userModel";
+import { CreateUserSchema, UserResponse, LoginUserSchema } from "./userModel";
 import { userService } from "./userService";
 import { ServiceResponse, handleServiceResponse } from "@/common/utils/serviceResponse";
 
@@ -7,6 +7,12 @@ class UserController {
     public createUser: RequestHandler = async (req: Request, res: Response) => {
         const data = CreateUserSchema.parse(req.body);
         const serviceResponse: ServiceResponse<UserResponse | null> = await userService.createUser(data);
+        return handleServiceResponse(serviceResponse, res);
+    };
+    
+    public loginUser: RequestHandler = async (req: Request, res: Response) => {
+        const data = LoginUserSchema.parse(req.body);
+        const serviceResponse: ServiceResponse<UserResponse | null> = await userService.loginUser(data);
         return handleServiceResponse(serviceResponse, res);
     };
 }
