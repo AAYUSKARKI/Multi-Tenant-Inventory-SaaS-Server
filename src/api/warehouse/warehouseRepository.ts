5111f3c9ba27dac444ba1c3565c2b15489fbab2c
aayus karki <karki.aayush2003@icloud.com>
2025-12-04 13:20:58 +0545

    feat(warehouse):add prisma query for update warehouse

--- src/api/warehouse/warehouseRepository.ts
+++ src/api/warehouse/warehouseRepository.ts
@@ -24,4 +24,14 @@ export class WarehouseRepository {
             },
         });
     }
+
+    async update(warehouseId: string, data: CreateWarehouse, tenantId: string): Promise<Warehouse> {
+        return prisma.warehouse.update({
+            where: {
+                id: warehouseId,
+                tenantId: tenantId,
+            },
+            data: data,
+        });
+    }
 }
